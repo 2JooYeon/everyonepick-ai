00e912343506ad01bba6cf34e05eb0ff0230c4a6
이주연 <lewndus7749@naver.com>
2022-11-06 01:34:43 +0900

    [PICK-137] REFACTOR: utils.py 파일 분리

---
+++ Face_Swap/utils.py
@@ -0,0 +1,28 @@
+# 가장 많은 선택을 받은 사진의 인덱스를 찾는 함수
+def find_base_photo_id(user_choices, photo_id_count):
+    # 아무도 선택 하지 않은 경우
+    if len(user_choices) == 0:
+        return -1
+
+    for choices in user_choices.values():
+        for choice in choices:
+            photo_id_count[choice] += 1
+
+    # 사진 별 선택 수 (내림차순 정렬)
+    sorted_photo_id_count = sorted(photo_id_count.items(), reverse=True, key=lambda item: item[1])
+    # base 사진, 받은 선택 수
+    base_photo_id = sorted_photo_id_count[0][0]
+
+    return base_photo_id
+
+
+# face swap이 필요한 user_id와 source 사진을 찾는 함수
+def list_of_face_swap(user_choices, base_photo_id):
+    user_id_choice = []
+
+    for user_id, choices in user_choices.items():
+        if base_photo_id not in choices:
+            # 첫 번째 선택 사진을 source 사진으로 결정
+            user_id_choice.append((user_id, choices[0]))
+
+    return user_id_choice
