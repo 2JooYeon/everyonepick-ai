a060a63f016aace9d53627636cd45498019c3c0d
이주연 <lewndus7749@naver.com>
2022-11-06 02:22:18 +0900

    [PICK-137] ADD: 합성이 필요 없을 경우 데이터 전송하는 코드 추가

--- Face_Swap/main.py
+++ Face_Swap/main.py
@@ -1,12 +1,14 @@
 from kafka import KafkaConsumer
 from json import loads
 import logging
-import traceback
 from input_data import refine_input
 from utils import *
+from producer import *
 
 consumer = KafkaConsumer("everyonepick.faceswap.request",
-                         bootstrap_servers=["b-1.everyonepickkafka.v7ao1r.c3.kafka.ap-northeast-2.amazonaws.com:9092", "b-3.everyonepickkafka.v7ao1r.c3.kafka.ap-northeast-2.amazonaws.com:9092", "b-2.everyonepickkafka.v7ao1r.c3.kafka.ap-northeast-2.amazonaws.com:9092"],
+                         bootstrap_servers=["b-1.everyonepickkafka.v7ao1r.c3.kafka.ap-northeast-2.amazonaws.com:9092",
+                                            "b-3.everyonepickkafka.v7ao1r.c3.kafka.ap-northeast-2.amazonaws.com:9092",
+                                            "b-2.everyonepickkafka.v7ao1r.c3.kafka.ap-northeast-2.amazonaws.com:9092"],
                          value_deserializer=lambda x: loads(x.decode('utf-8')),
                          auto_offset_reset="earliest",
                          enable_auto_commit=False,
@@ -21,6 +23,11 @@ while True:
             try:
                 pick_id, photo_id_url, photo_id_count, user_choices, user_embedding = refine_input(record)
                 target_img_id = find_base_photo_id(user_choices, photo_id_count)
+                face_swap_list = list_of_face_swap(user_choices, target_img_id)
+                # 합성이 필요 없는 경우
+                if len(face_swap_list) == 0:
+                    img = download_s3_url(photo_id_url[target_img_id])
+                    send_data(pick_id, img)
 
             except:
                 logging.error(traceback.format_exc())
