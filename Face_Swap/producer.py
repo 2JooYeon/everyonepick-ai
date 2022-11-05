1fbd2164f1c13d51c598cae840c8c3fde21908ca
이주연 <lewndus7749@naver.com>
2022-11-06 02:13:26 +0900

    [PICK-137] ADD: pick_id와 base64 인코딩된 이미지 전송 코드 추가

--- Face_Swap/producer.py
+++ Face_Swap/producer.py
@@ -1,6 +1,8 @@
 from kafka import KafkaProducer
 from json import dumps
 import traceback
+import base64
+import cv2
 
 producer=KafkaProducer(
     bootstrap_servers=["b-1.everyonepickkafka.v7ao1r.c3.kafka.ap-northeast-2.amazonaws.com:9092",
@@ -9,11 +11,16 @@ producer=KafkaProducer(
     value_serializer=lambda x: dumps(x).encode('utf-8')
 )
 
-record = [{"pick_id":1, "image":"byte code"}]
-try:
-    response = producer.send(topic='everyonepick.faceswap.result', value=record).get()
-    print(response)
-    producer.flush()
-except:
-    traceback.print_exc()
+def send_data(pick_id, np_img):
+    record = {}
+    record["pick_id"] = pick_id
+
+    # numpy 형식의 이미지를 base64 인코딩하기
+    base64_img = base64.b64encode(cv2.imencode('.jpg', np_img)[1]).decode()
+    record["image"] = base64_img
+    try:
+        response = producer.send(topic='everyonepick.faceswap.result', value=record).get()
+        producer.flush()
+    except:
+        traceback.print_exc()
 
