01c5474659690b1f1e37f4a502f8243b9c40fcdb
이주연 <lewndus7749@naver.com>
2022-11-06 02:21:43 +0900

    [PICK-137] MODIFY: 이미지 반환 키값 이름 변경

--- Face_Swap/producer.py
+++ Face_Swap/producer.py
@@ -17,7 +17,7 @@ def send_data(pick_id, np_img):
 
     # numpy 형식의 이미지를 base64 인코딩하기
     base64_img = base64.b64encode(cv2.imencode('.jpg', np_img)[1]).decode()
-    record["image"] = base64_img
+    record["face_swap_result"] = base64_img
     try:
         response = producer.send(topic='everyonepick.faceswap.result', value=record).get()
         producer.flush()
