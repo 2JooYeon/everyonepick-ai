c13df9a2c24d619ab82b55efde4b0c5f9c95f877
이주연 <lewndus7749@naver.com>
2022-11-08 07:14:44 +0900

    [PICK-137] FIX: find_user_face 인자 수정

--- Face_Swap/face_swap.py
+++ Face_Swap/face_swap.py
@@ -20,14 +20,17 @@ def swap_face(user_face_embedding, source_img, target_img, target_faces):
     rotate_angle = source_angle - target_angle
     source_img_rotated = imutils.rotate_bound(source_img, rotate_angle)
 
+    source_faces = get_faceobj(source_img_rotated)
     '''target 얼굴 크기에 맞춰 source 얼굴 크기 조정하기'''
-    source_face = find_user_face(source_img_rotated, user_face_embedding)
+    source_face = find_user_face(source_faces, user_face_embedding)
     source_img_resized = resize_img(source_face, target_face, source_img_rotated)
 
     '''segmentation 진행'''
     source_seg_label = get_seg_label(source_img_resized, source_face)
     target_seg_label = get_seg_label(target_img, target_face)
 
+    source_faces = get_faceobj(source_img_resized)
+    source_face = find_user_face(source_faces, user_face_embedding)
 
     source_position, target_position = adjust_swap_position(source_seg_label, source_face, target_face, target_img)
     s_start_x, s_start_y, s_end_x, s_end_y = source_position
@@ -37,17 +40,13 @@ def swap_face(user_face_embedding, source_img, target_img, target_faces):
 
     '''target 이미지 크기와 동일한 마스크 생성(3차원)'''
     source_face_mask = np.zeros(target_img.shape, np.uint8)
-    source_face_mask[t_start_y:t_end_y + 1, t_start_x:t_end_x + 1] = source_face_image[
-                                                                     s_start_y:s_end_y + 1,
-                                                                     s_start_x:s_end_x + 1]
+    source_face_mask[t_start_y:t_end_y, t_start_x:t_end_x] = source_face_image[s_start_y:s_end_y, s_start_x:s_end_x]
 
     '''target 이미지 크기와 동일한 마스크 생성(1차원)'''
     target_img_gray = cv2.cvtColor(target_img, cv2.COLOR_BGR2GRAY)
     source_seg_mask = np.zeros_like(target_img_gray)
 
-    source_seg_mask[t_start_y:t_end_y + 1, t_start_x:t_end_x + 1] = source_seg_label[
-                                                                    s_start_y:s_end_y + 1,
-                                                                    s_start_x:s_end_x + 1]
+    source_seg_mask[t_start_y:t_end_y, t_start_x:t_end_x] = source_seg_label[s_start_y:s_end_y,s_start_x:s_end_x]
 
     '''얼굴에 해당하는 픽셀만 255, 나머지 0'''
     source_face_seg = np.where((0 < source_seg_mask) & (source_seg_mask < 10), 255, 0)
